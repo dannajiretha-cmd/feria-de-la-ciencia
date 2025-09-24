ca14fb06797cffca319551fa46cb51c99c8465b3
dannajiretha-cmd <dannajiretha@gmail.com>
2025-09-23 22:46:59 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     SerialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     if (SerialData == "Iniciar") {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
         basic.showIcon(IconNames.Yes)
     } else if (SerialData == "Detener") {
         SuperBit.MotorStopAll()
