7cb90b51ed73e7d9515c79e2f129413ca1b23b27
dannajiretha-cmd <dannajiretha@gmail.com>
2025-09-23 22:37:56 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    SerialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    if (SerialData == "Iniciar") {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+        basic.showIcon(IconNames.Yes)
+    } else if (SerialData == "Detener") {
+        SuperBit.MotorStopAll()
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (SerialData == "Fondo") {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+    	
+    }
+})
+let SerialData = ""
+serial.redirectToUSB()
